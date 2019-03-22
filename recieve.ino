7cf2e6a125e4842cd83178a8eafca45bef9d7ccc
Nimish Bongale <nimishnb98@gmail.com>
2019-03-22 20:51:39 +0530

    Initial Commit

---
+++ recieve.ino
@@ -0,0 +1,38 @@
+#include <VirtualWire.h>
+const int receive_pin = 2;
+const int relay=3;
+bool relayState =false;
+void setup()
+{   
+    pinMode(relay,OUTPUT);
+    Serial.begin(9600);  // Debugging only
+    Serial.println("setup");
+    vw_set_rx_pin(receive_pin);
+    
+    vw_setup(2000);  // Bits per sec
+
+    vw_rx_start();       // Start the receiver PLL running
+}
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+(relayState==true)?(digitalWrite(13,HIGH)):(digitalWrite(13,LOW));
+(relayState==true)?(digitalWrite(relay,LOW)):(digitalWrite(relay,HIGH));
+      
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+  
+      Serial.println(buf[0]);
+      
+      if(buf[0]==49){
+        relayState=false;
+        }
+       else if(buf[0]==48){
+          relayState=true;
+        }
+      }
+    
+
+}
---
+++ transmit.ino
@@ -0,0 +1,51 @@
+#include <VirtualWire.h>
+const int transmit_pin = 2;
+int switch_pin1=7;
+int switch_pin2=9;
+int switch_pin3=8;
+ void setup(){
+  Serial.begin(9600);
+  pinMode(switch_pin2,INPUT_PULLUP);
+  pinMode(switch_pin1,INPUT_PULLUP);
+  pinMode(switch_pin3,INPUT_PULLUP);
+  vw_set_tx_pin(transmit_pin);
+  vw_setup(2000);   // Bits per sec
+ }
+void loop(){
+  int mode=0;
+  int val1=digitalRead(switch_pin1);
+  int val2=digitalRead(switch_pin2);
+  int val3=digitalRead(switch_pin3);
+  Serial.print(val1);
+  Serial.print(",");
+  Serial.print(val2);
+  Serial.print(",");
+  Serial.println(val3);
+  if(val1!=0||val2!=0||val3!=0){
+    alarm(1);
+  }
+  else{
+    alarm(0);
+  }
+  delay(100);
+}
+
+
+void alarm(bool flag){
+if(flag==true){
+char *msg = "1"; 
+digitalWrite(13, HIGH); // Flash a light to show transmitting
+  vw_send((uint8_t *)msg,strlen (msg));
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(13, LOW);
+  delay(100); 
+}
+if(flag==false){
+ char *msg = "0"; 
+
+  vw_send((uint8_t *)msg,strlen (msg));
+  vw_wait_tx(); // Wait until the whole message is gone
+ delay(100); 
+  }
+
+}
